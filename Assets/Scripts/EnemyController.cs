46e52efa7a2b316280a3f4e515fb7c8ddd0e020c
DisguisedFox <diogo100089079@gmail.com>
2017-11-08 15:02:13 +0100

    Init projet

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField] private Transform[] gunsTransformList;
+    [SerializeField] private float time2Fire = 2.0f;
+    [SerializeField] private float bulletVelocity = 8.0f;
+    [SerializeField] private GameObject bulletPrefab;
+
+    private float life = 3;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Fire());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private IEnumerator Fire()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(time2Fire);
+            foreach(Transform t in gunsTransformList)
+            {
+                GameObject bullet = Instantiate(bulletPrefab, t.position, t.rotation);
+                bullet.GetComponent<Rigidbody2D>().velocity = t.right * bulletVelocity;
+                Destroy(bullet, 5);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Bullet")
+        {
+            life--;
+            if (life == 0)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    private int lifes = 3;
+
+    [SerializeField] private Text textLifes;
+
+    private const string TEXT_LIFES = "Lifes : ";
+
+    // Use this for initialization
+    void Start () {
+        textLifes.text = TEXT_LIFES + lifes;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayerDie()
+    {
+        lifes--;
+        if(lifes > 0)
+        {
+            textLifes.text = TEXT_LIFES + lifes;
+        }
+        else
+        {
+            SceneManager.LoadScene("DieMenu");
+        }
+    }
+
+}
